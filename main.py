80d2117535cdbe70fafdf95ea64c896ed34f3aa2
LukasWroclaw <66529653+LukasWroclaw@users.noreply.github.com>
2022-02-07 13:32:46 +0100

    Create main.py
    Creation

---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 10:55:19 2022
+
+@author: marszale
+"""
+
+from httpRequestHandler import httpRequestHandler
+from apiRequestBuilder import apiRequestBuilder
