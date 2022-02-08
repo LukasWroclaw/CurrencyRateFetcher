3c2eec3142515434f6dbc35ab0fe59ff0f9363ce
LukasWroclaw <66529653+LukasWroclaw@users.noreply.github.com>
2022-02-08 09:31:33 +0100

    Extention of httpRequest
    Extention of httpRequest

---
+++ getDataFromJson.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 08:36:38 2022
+
+@author: marszale
+"""
+import json
+
+
+
+
+
+
+
--- httpRequestHandler.py
+++ httpRequestHandler.py
@@ -5,8 +5,10 @@ Created on Mon Feb  7 11:31:10 2022
 @author: marszale
 """
 
-import requests
+import unittest
 
+import requests
+testuj = 1
     
 class httpRequestHandler(object):
     
@@ -18,4 +20,35 @@ class httpRequestHandler(object):
             return ""
             
         return response.json()
+    
+    def convertJsonToDict(self, jsonInput):
+        dictForRates = {}
+        
+        rates = jsonInput["rates"]
+        
+        for element in rates:
+            date = str(element['effectiveDate'])
+            value = float(element['mid'])
+            dictForRates[date] = value
+            
+        return dictForRates
+            
+        
 
+
+class TestingClass(unittest.TestCase):
+  
+    
+    def test_basic1(self):
+            requestText = "http://api.nbp.pl/api/exchangerates/rates/a/eur/2021-01-04/2021-01-07/"
+            requestHandler = httpRequestHandler()
+            receivedJson = requestHandler.getJsonFileFromUrl(requestText)
+            dictionary = requestHandler.convertJsonToDict(receivedJson)
+            expectedDict = {'2021-01-04': 4.5485, '2021-01-05': 4.5446, '2021-01-07': 4.4973}
+            self.assertEqual(dictionary, expectedDict)
+
+
+
+if(testuj):
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestingClass)
+    unittest.TextTestRunner(verbosity=2).run(suite)      
