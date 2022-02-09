edd46bd522f64d1c0bfe1ff189691d89cb60b597
LukasWroclaw <66529653+LukasWroclaw@users.noreply.github.com>
2022-02-09 13:17:10 +0100

    AddingHandlingOfCSV

--- getDataFromCSV.py
+++ getDataFromCSV.py
@@ -2,10 +2,12 @@
 """
 Created on Tue Feb  8 12:16:22 2022
 
+
+
 @author: marszale
 """
 
-from datetime import date
+
 import csv
 import unittest
 
@@ -24,27 +26,93 @@ class getDataFromCSVhandler(object):
     def convertDate(self, rawDate):
       
         splitted = rawDate.split(".")
-        day = int(splitted[0])
-        month = int(splitted[1])
-        trimmedYear = int((splitted[2])[:4])
         
-        return date(trimmedYear,month,day)
+        dayInt = int(splitted[0])
+        
+        if dayInt < 10:
+            day = "0" + str(dayInt)
+        else:
+            day = str(dayInt)
+            
+            
+        monthInt = int(splitted[1])
+        
+        if monthInt < 10:
+            month = "0" + str(monthInt)
+        else:
+            month = str(monthInt)
         
         
-"""
-"10.01.2022  12:11:00"
-
-with open('tableForTest.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+        trimmedYear = str((splitted[2])[:4])
+        
+        return trimmedYear + "-"  + month + "-" + day
     
-    for row in csv_reader:
-        print(row)
+    def getDataFromFile(self, fileName):
+        with open(fileName, mode='r') as csv_file:
+            incr = 0;
+            listOfRows = []
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                listOfRows.append(row)
+                incr+=1
+                
+            return listOfRows
+
+    def addOrUpdateElementInDictionary(self, dictionary, key, value):
+        if key in dictionary:
+             dictionary[key] = dictionary[key] + value
+        else:
+             dictionary[key] = value
+    
+    def getConvertedData(self, listOfRows):
+        dictionaryWithIncome = {}
         
-"""        
+       
+        for row in listOfRows:           
+           if self.isIncome(row["Details"]):
+               key = self.convertDate(row["Date"])
+               self.addOrUpdateElementInDictionary(dictionaryWithIncome, key, float(row["Turnover"]))
+              
+          
+        return dictionaryWithIncome
+        
+        
+    
         
         
 class TestingClass(unittest.TestCase):
   
+    def test_getConvertedData1(self):
+            fileName = 'tableForTestShort.csv'
+            handler = getDataFromCSVhandler()
+            listWithData = handler.getDataFromFile(fileName)
+            incomeDictionary = handler.getConvertedData(listWithData)
+            incomeForParticularDay = incomeDictionary['2022-01-01']
+            self.assertEqual(incomeForParticularDay, 0.082874284)
+
+
+
+    def test_getConvertedData2(self):
+            fileName = 'tableForTest.csv'
+            handler = getDataFromCSVhandler()
+            listWithData = handler.getDataFromFile(fileName)
+            incomeDictionary = handler.getConvertedData(listWithData)
+            incomeForParticularDay = incomeDictionary['2022-01-07']
+            self.assertEqual(incomeForParticularDay, 0.113679132)
+  
+    
+    def test_getDataFromFile1(self):
+            fileName = 'tableForTestShort.csv'
+            handler = getDataFromCSVhandler()
+            listWithData = handler.getDataFromFile(fileName)
+            self.assertEqual(len(listWithData), 5)
+            
+    def test_getDataFromFile2(self):
+            fileName = 'tableForTestShort.csv'
+            handler = getDataFromCSVhandler()
+            listWithData = handler.getDataFromFile(fileName)
+            secondRow = listWithData[1]
+            self.assertEqual(secondRow['Transaction ID:'], '2')
     
     def test_isIncome1(self):
             detailsText = "Loan 4 - investment in loan "
@@ -66,7 +134,7 @@ class TestingClass(unittest.TestCase):
     def test_dateConversion1(self):
             rawDateText = "10.01.2022  12:11:00"
             handler = getDataFromCSVhandler()
-            self.assertEqual(handler.convertDate(rawDateText), date(2022, 1, 10))
+            self.assertEqual(handler.convertDate(rawDateText), "2022-01-10")
 
 
 if(testuj):
